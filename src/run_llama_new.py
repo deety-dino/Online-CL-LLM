a085528f4a2fbee1800893c7ba1e8ccbe7b56908
Đức Anh <24021369@vnu.edu.vn>
2026-08-09 22:07:36 +0700

    Update run_llama_new.py

--- src/run_llama_new.py
+++ src/run_llama_new.py
@@ -721,8 +721,8 @@ def main():
                     }) + "\n")
         return result
     print(f"-----Gradient checkpointing: {training_args.gradient_checkpointing} -----")
-    if training_args.gradient_checkpointing:
-        model.gradient_checkpointing_enable()
+    #if training_args.gradient_checkpointing:
+    #    model.gradient_checkpointing_enable()
 
     world_size = int(os.environ.get("WORLD_SIZE", 1))
     training_args.step_per_epoch = math.ceil(len(raw_datasets["train"]) / training_args.per_device_train_batch_size / world_size / training_args.gradient_accumulation_steps)
