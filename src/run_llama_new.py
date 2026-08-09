3ee1f43e32be53817ccffd0716a8689c5418feb9
Đức Anh <24021369@vnu.edu.vn>
2026-08-09 23:34:33 +0700

    Update run_llama_new.py

--- src/run_llama_new.py
+++ src/run_llama_new.py
@@ -495,7 +495,7 @@ def main():
         'flash_attention':model_args.flash_attention,
         'successor':model_args.successor
     }
-    tensor_quant.QUANT_DESC_WT_INT8.weight_quantizer.num_bits = 8
+    tensor_quant.QUANT_DESC_WEIGHT_INT8.weight_quantizer.num_bits = 8
     tensor_quant.QUANT_DESC_INPUT_INT8.input_quantizer.num_bits = 8
     quant_modules.initialize()
     quant_nn.TensorQuantizer.use_quant_default = True
