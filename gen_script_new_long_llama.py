fd06bc2be3fd8aa1432dc94749b1c71f3c4d7f08
Đức Anh <24021369@vnu.edu.vn>
2026-08-10 17:46:28 +0700

    Update gen_script_new_long_llama.py

--- gen_script_new_long_llama.py
+++ gen_script_new_long_llama.py
@@ -1,77 +1,43 @@
 import json
-def load_json(path):
-    with open(path, 'r', encoding='utf-8') as f:
-        data = json.load(f)
-    return data
-
-def write_json(path, data):
-    with open(path, 'w', encoding='utf-8') as f:
-        json.dump(data, f, ensure_ascii=False,indent=2)
-
-def load_jsonline(path):
-    with open(path, 'r', encoding='utf-8') as f:
-        result=[]
-        for line_s in f:
-            line=json.loads(line_s)
-            result.append(line)
-    return result
-
-def write_jsonline(path, data):
-    with open(path, 'w', encoding='utf-8') as f:
-        for line in data:
-            line_s=json.dumps(line, ensure_ascii=False)
-            f.write(line_s)
-            f.write('\n')
-
-order_idx = 3
-
-if order_idx == 4:
-    all_tasks=[
-        "yelp",
-        "amazon",
-        "mnli",
-        "cb",
-        "copa",
-        "qqp",
-        "rte",
-        "imdb",
-        "sst2",
-        "dbpedia",
-        "agnews",
-        "yahoo",
-        "multirc",
-        "boolq",
-        "wic"
-    ] # Order 4
-else:
-    all_tasks = ["mnli",
-                 "cb",
-                 "wic",
-                 "copa",
-                 "qqp",
-                 "boolq",
-                 "rte",
-                 "imdb",
-                 "yelp",
-                 "amazon",
-                 "sst2",
-                 "dbpedia",
-                 "agnews",
-                 "multirc",
-                 "yahoo"] # Order 3
-
-dataset_list = all_tasks
-task_order = ','.join(all_tasks)
-
-config_template={
-    "Long_Sequence": [
-    ],
-}
-
 import os
 import pathlib
 import numpy as np
 from copy import deepcopy
+
+def load_json(path):
+    with open(path, 'r', encoding='utf-8') as f:
+        return json.load(f)
+
+def write_json(path, data):
+    with open(path, 'w', encoding='utf-8') as f:
+        json.dump(data, f, ensure_ascii=False, indent=2)
+
+def load_jsonline(path):
+    with open(path, 'r', encoding='utf-8') as f:
+        return [json.loads(line_s) for line_s in f]
+
+def write_jsonline(path, data):
+    with open(path, 'w', encoding='utf-8') as f:
+        for line in data:
+            f.write(json.dumps(line, ensure_ascii=False) + '\n')
+
+order_idx = 3
+
+if order_idx == 4:
+    all_tasks = [
+        "yelp", "amazon", "mnli", "cb", "copa", "qqp", "rte", 
+        "imdb", "sst2", "dbpedia", "agnews", "yahoo", "multirc", "boolq", "wic"
+    ]
+else:
+    all_tasks = [
+        "mnli", "cb", "wic", "copa", "qqp", "boolq", "rte", 
+        "imdb", "yelp", "amazon", "sst2", "dbpedia", "agnews", "multirc", "yahoo"
+    ]
+
+dataset_list = all_tasks
+task_order = ','.join(all_tasks)
+
+config_template = {"Long_Sequence": []}
 
 lora_r = 4
 lora_alpha = 32
@@ -83,49 +49,58 @@ num_train_epochs = 3
 attn_lr = 0.
 replay_after_n_epoch = 0
 
+distances_temperature = 1.0
+distances_way = 'Attention'
+train_top = 1
+test_top = train_top
+train_top_p = -1.0
+test_top_p = -1.0
 
-distances_temperature=1.0
-distances_way='Attention'
-train_top=1
-test_top=train_top
-train_top_p=-1.0
-test_top_p=-1.0
-
-successor='N'
+successor = 'N'
 
 run_name = f"test_llama_7b_long_our_8_1_4_{distances_way}_{distances_temperature}_train_top_{train_top}_test_top_{test_top}_train_top_p_{train_top_p}_test_top_p_{test_top_p}"
-model_path='meta-llama/Llama-2-7b-chat-hf'
+model_path = 'meta-llama/Llama-2-7b-chat-hf'
 
-history_config=[]
+history_config = []
 for one_data_name in dataset_list:
-
     pathlib.Path(f'./configs/{run_name}_configs/{one_data_name}').mkdir(parents=True, exist_ok=True)
 
-    config={
+    config = {
         "sampling strategy": "full",
         "dataset name": f"{one_data_name}"
     } 
     history_config.append(config)
 
-    dev_config=deepcopy(config_template)
+    dev_config = deepcopy(config_template)
     dev_config['Long_Sequence'].append(config)
     write_json(f'./configs/{run_name}_configs/{one_data_name}/dev_tasks.json', dev_config)
     
-    train_config=deepcopy(config_template)
+    train_config = deepcopy(config_template)
     train_config['Long_Sequence'].append(config)
     write_json(f'./configs/{run_name}_configs/{one_data_name}/train_tasks.json', train_config)
 
-    test_config=deepcopy(config_template)
+    test_config = deepcopy(config_template)
     test_config['Long_Sequence'].extend(history_config)
     write_json(f'./configs/{run_name}_configs/{one_data_name}/test_tasks.json', test_config)
 
-sh_str=rf'''
+# Tham số tối ưu RAM/VRAM dùng chung
+ram_optimized_args = rf'''
+   --bf16 True \
+   --gradient_checkpointing True \
+   --optim paged_adamw_8bit \
+   --dataloader_num_workers 2 \
+   --per_device_train_batch_size 1 \
+   --per_device_eval_batch_size 2 \
+   --gradient_accumulation_steps 4 \
+'''
+
+sh_str = rf'''
 #!/bin/bash
 #SBATCH -J cl                           
 #SBATCH -o cl-%j.out                       
 #SBATCH -p compute 
 #SBATCH -N 1                           
-#SBATCH -t 20:00:00   
+#SBATCH -t 20:00:00    
 #SBATCH --mem 128G 
 #SBATCH --gres=gpu:a100-sxm4-80gb:1
 
@@ -143,17 +118,14 @@ deepspeed --num_gpus=1 src/run_llama_new.py \
    --data_dir CL_Benchmark \
    --task_order {task_order} \
    --task_config_dir configs/{run_name}_configs/{dataset_list[0]} \
-   --output_dir logs_and_outputs/{run_name}/outputs/1-{dataset_list[0]} \
-   --per_device_train_batch_size 1 \
-   --per_device_eval_batch_size 8 \
-   --gradient_accumulation_steps 4 \
+   --output_dir logs_and_outputs/{run_name}/outputs/1-{dataset_list[0]} \{ram_optimized_args}
    --learning_rate {learning_rate} \
    --attn_lr {attn_lr} \
    --num_train_epochs {num_train_epochs} \
    --run_name {run_name} \
    --distances_temperature {distances_temperature} \
    --distances_way {distances_way} \
-   --max_source_length 1024 \
+   --max_source_length 512 \
    --max_target_length 50 \
    --generation_max_length 50 \
    --add_task_name False \
@@ -206,7 +178,7 @@ for idx in range(len(dataset_list)-1):
         else:
             max_steps = 500
         
-        sh_str+=rf'''
+        sh_str += rf'''
 
 deepspeed --num_gpus=1 src/run_llama_new.py \
    --do_train \
@@ -219,17 +191,14 @@ deepspeed --num_gpus=1 src/run_llama_new.py \
    --task_order {task_order} \
    --gen_data_dir generated_data/lora_gen_15datasets_t5_xl \
    --task_config_dir configs/{run_name}_configs/{dataset_list[idx+1]} \
-   --output_dir logs_and_outputs/{run_name}/outputs/{idx+2}-{dataset_list[idx+1]} \
-   --per_device_train_batch_size 1 \
-   --per_device_eval_batch_size 8 \
-   --gradient_accumulation_steps 4 \
+   --output_dir logs_and_outputs/{run_name}/outputs/{idx+2}-{dataset_list[idx+1]} \{ram_optimized_args}
    --learning_rate {learning_rate} \
    --attn_lr {attn_lr} \
    --max_steps {max_steps} \
    --run_name {run_name} \
    --distances_temperature {distances_temperature} \
    --distances_way {distances_way} \
-   --max_source_length 1024 \
+   --max_source_length 512 \
    --max_target_length 50 \
    --generation_max_length 50 \
    --add_task_name False \
@@ -262,7 +231,7 @@ rm -rf logs_and_outputs/{run_name}/outputs/{idx+2}-{dataset_list[idx+1]}/checkpo
 
 '''
     else:
-        sh_str+=rf'''
+        sh_str += rf'''
 
 deepspeed --num_gpus=1 src/run_llama_new.py \
    --do_train \
@@ -275,17 +244,14 @@ deepspeed --num_gpus=1 src/run_llama_new.py \
    --task_order {task_order} \
    --gen_data_dir generated_data/lora_gen_long_llama \
    --task_config_dir configs/{run_name}_configs/{dataset_list[idx+1]} \
-   --output_dir logs_and_outputs/{run_name}/outputs/{idx+2}-{dataset_list[idx+1]} \
-   --per_device_train_batch_size 1 \
-   --per_device_eval_batch_size 8 \
-   --gradient_accumulation_steps 4 \
+   --output_dir logs_and_outputs/{run_name}/outputs/{idx+2}-{dataset_list[idx+1]} \{ram_optimized_args}
    --learning_rate {learning_rate} \
    --attn_lr {attn_lr} \
    --num_train_epochs {num_train_epochs} \
    --run_name {run_name} \
    --distances_temperature {distances_temperature} \
    --distances_way {distances_way} \
-   --max_source_length 1024 \
+   --max_source_length 512 \
    --max_target_length 50 \
    --generation_max_length 50 \
    --add_task_name False \
@@ -318,8 +284,7 @@ rm -rf logs_and_outputs/{run_name}/outputs/{idx+2}-{dataset_list[idx+1]}/checkpo
 
 '''
 
-
-sh_str+=rf'''
+sh_str += rf'''
 
 deepspeed --num_gpus=1 src/run_llama_new_eval.py \
    --do_predict \
@@ -331,17 +296,14 @@ deepspeed --num_gpus=1 src/run_llama_new_eval.py \
    --task_order {task_order} \
    --gen_data_dir generated_data/lora_gen_long_llama \
    --task_config_dir configs/{run_name}_configs/{dataset_list[idx+1]} \
-   --output_dir logs_and_outputs/{run_name}/outputs/{idx+2}-{dataset_list[idx+1]} \
-   --per_device_train_batch_size 1 \
-   --per_device_eval_batch_size 8 \
-   --gradient_accumulation_steps 4 \
+   --output_dir logs_and_outputs/{run_name}/outputs/{idx+2}-{dataset_list[idx+1]} \{ram_optimized_args}
    --learning_rate {learning_rate} \
    --attn_lr {attn_lr} \
    --num_train_epochs {num_train_epochs} \
    --run_name {run_name} \
    --distances_temperature {distances_temperature} \
    --distances_way {distances_way} \
-   --max_source_length 1024 \
+   --max_source_length 512 \
    --max_target_length 50 \
    --generation_max_length 50 \
    --add_task_name False \
@@ -370,6 +332,6 @@ deepspeed --num_gpus=1 src/run_llama_new_eval.py \
    --test_key_weight_top_p {test_top_p} \
    --successor {successor}
 '''
-    
+
 with open(f'{run_name}.sh', 'w') as f:
     f.write(sh_str)
