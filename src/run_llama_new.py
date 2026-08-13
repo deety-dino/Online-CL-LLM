c37375f561875276493db24c5856cb0de6d303de
Đức Anh <24021369@vnu.edu.vn>
2026-08-13 10:16:57 +0700

    Update run_llama_new.py

--- src/run_llama_new.py
+++ src/run_llama_new.py
@@ -500,7 +500,7 @@ def main():
         revision=model_args.model_revision,
         use_auth_token=True if model_args.use_auth_token else None,
         use_safetensors=True,
-    ).to('cuda')
+    )
     
     model.resize_token_embeddings(len(tokenizer))
 
