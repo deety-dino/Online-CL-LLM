11b7932c406794e0a39496b92ad9dfd691c0779d
Đức Anh <24021369@vnu.edu.vn>
2026-08-15 18:19:31 +0700

    Update gen_script_new_long_llama.py

--- gen_script_new_long_llama.py
+++ gen_script_new_long_llama.py
@@ -333,7 +333,7 @@ deepspeed --num_gpus=2 src/run_llama_new.py \
    --gen_data_dir generated_data/lora_gen_long_llama \
    --task_config_dir configs/{run_name}_configs/{dataset_list[idx+1]} \
    --output_dir logs_and_outputs/{run_name}/outputs/{idx+2}-{dataset_list[idx+1]} \
-   --per_device_train_batch_size 2 \
+   --per_device_train_batch_size 1 \
    --per_device_eval_batch_size 8 \
    --gradient_accumulation_steps 4 \
    --learning_rate {learning_rate} \
